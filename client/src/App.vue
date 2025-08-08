f6ecc3f2db3f403d0064bef42cfc75bac53ddcf4
yazanalhayek <yazzhayek1998@gmail.com>
2025-08-08 14:48:09 +0200

    style

--- client/src/App.vue
+++ client/src/App.vue
@@ -3,8 +3,6 @@ import {ref} from 'vue'
 import axios from 'axios'
 
 
-
-
 const wait = ref<boolean>(false)
 const logoName = ref('')
 const data = ref('')
@@ -33,7 +31,7 @@ async function generateQr() {
     const response = await axios.post(`${import.meta.env.VITE_API_URL}/generate-qr/`, formData, {
       responseType: 'blob',
     })
-    console.log(response)
+
 
     qrUrl.value = URL.createObjectURL(response.data)
   } catch (error) {
@@ -43,17 +41,20 @@ async function generateQr() {
         const blob = error.response.data
         const text = await blob.text() // Lees de error als tekst
         console.error('Serverfout:', text)
-        alert(`Fout van server: ${text}`)
+        error.value = `Fout van server: ${text}`
       } else {
         // Verbindingsfout of iets anders
         console.error('Verbindingsfout of onbekende fout:', error.message)
-        alert(`Verbindingsfout: ${error.message}`)
+        error.value = `Verbindingsfout: ${error.message}`
       }
     } else {
       console.error('Onbekende fout:', error)
+      error.value = `Er is een onbekende fout opgetreden.`
       alert('Er is een onbekende fout opgetreden.')
+
     }
   } finally {
+    error.value = null
     wait.value = false
   }
 }
@@ -61,13 +62,13 @@ async function generateQr() {
 </script>
 
 <template>
-  <div class="flex justify-center items-center">
+  <div class="">
     <div class="bg-gray-100 min-h-lvh -z-10 absolute top-0 left-0 w-[100vw] flex justify-center items-center">
-      <img src="../public/icon.png" alt="logo" class=" blur-xl w-1/2"/>
+      <img src="./assets/Icon.svg" alt="logo" class=" blur-xl w-1/2"/>
     </div>
 
     <div
-        class="z-100 max-h-lvh  w-[80vw]  flex flex-col items-center justify-center  p-6 bg-white/25 backdrop-blur-xl">
+        class="z-100 max-h-lvh p-6 flex flex-col items-center justify-center   bg-white/25 backdrop-blur-xl">
       <h1 class="text-2xl font-bold mb-4">QR-code Generator</h1>
       <form @submit.prevent="generateQr" class="flex flex-col j gap-10 space-y-4 w-full max-w-sm">
         <input
@@ -108,14 +109,17 @@ async function generateQr() {
           <span v-if="!wait">Genereer QR-code</span>
         </button>
       </form>
-      <strong v-if="error" v-text="error"></strong>
-      <div v-if="qrUrl" class=" mt-6 text-center">
-        <h2 class="mb-2 font-semibold">QR-code:</h2>
-        <img :src="qrUrl" alt="QR Code" class="border rounded-xl"/>
-        <a :href="qrUrl" download="qr-code.png" class="mt-6 inline-block text-gray-600 border p-1 px-3 rounded-2xl">
-          Download
-        </a>
-      </div>
+    </div>
+    <div v-if="error" class=" mt-6 text-red-50 text-center z-100 max-h-lvh p-6 flex flex-col items-center justify-center   bg-white/25 backdrop-blur-xl">
+      <strong  v-text="error"></strong>
+    </div>
+
+    <div v-if="qrUrl" class=" mt-6 text-center z-100 max-h-lvh p-6 flex flex-col items-center justify-center   bg-white/25 backdrop-blur-xl">
+      <h2 class="mb-2 font-semibold">QR-code:</h2>
+      <img :src="qrUrl" alt="QR Code" class="border rounded-xl"/>
+      <a :href="qrUrl" download="qr-code.png" class="mt-6 inline-block text-gray-600 border p-1 px-3 rounded-2xl">
+        Download
+      </a>
     </div>
   </div>
 </template>
